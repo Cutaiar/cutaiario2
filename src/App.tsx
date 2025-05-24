9a8588a6932121d5eed300f6638c5b1431869952
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-05-23 22:38:12 -0600

    focus visible users hover style

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ const A = ({ href, children }: { href: string; children: React.ReactNode }) => {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-amber-700 hover:text-amber-500 hover:underline underline-offset-2 transition-colors duration-100"
+      className="text-amber-700 hover:text-amber-500 hover:underline underline-offset-2 transition-colors duration-100 outline-none focus-visible:text-amber-500 focus-visible:underline"
     >
       {children}
     </a>
