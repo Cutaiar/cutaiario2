b29ddf588921b3d2ac036f165e1ec7b06569d8d5
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-05-24 00:04:00 -0600

    Fix typos

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,20 +5,20 @@ export const App = () => {
   return (
     <main className="bg-gray-100 flex flex-col items-center justify-center size-full font-anon">
       <CheekySun />
-      <div className="space-y-4 container p-8 w-fit overflow-y-auto animate-in fade-in ease-in-cubic duration-[700ms]">
+      <div className="space-y-4 p-8 w-fit overflow-y-auto animate-in fade-in ease-in-cubic duration-[700ms]">
         <h1 className="text-2xl font-bold">DILLON CUTAIAR</h1>
         <p className="max-w-prose text-justify">
           I'm a <strong>Software Engineer</strong> and <strong>Creative</strong>
           . After studying Computer Science, Philosophy, and Mathematics at
-          Virginia Tech, I worked as a full stack web developer at Microsoft
-          building the future of CRM with a lovely team. After a career break
-          spent running, climbing, and skiing, I joined the world of startups
-          with <A href="https://backdropbuild.com">Backdrop Build</A>. Here, I
-          worked with <A href="https://github.com/nebolax">Alexey</A> on{" "}
+          Virginia Tech, I worked as a full stack engineer at Microsoft building
+          the future of CRM with a lovely team. After a career break spent
+          running, climbing, and skiing, I joined the world of startups with{" "}
+          <A href="https://backdropbuild.com">Backdrop Build</A>. Here, I worked
+          with <A href="https://github.com/nebolax">Alexey</A> on{" "}
           <A href={"https://github.com/smoothusdt/smooth-pwa"}>SmoothUSDT</A> -
           a PWA wallet for USDT on Tron. Now, I'm leading engineering at{" "}
           <A href={"https://mysorbet.xyz"}>Sorbet</A> where I build, maintain,
-          and a deploy a wallet for freelancers designed for effortless global
+          and deploy a wallet for freelancers designed for effortless global
           payment.
         </p>
         <div className="flex flex-col justify-between sm:flex-row gap-4">
